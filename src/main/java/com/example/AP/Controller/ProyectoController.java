a99408ea0814b51b9d26ce210a19a550d8b2217f
Sole1988 <94577357+Sole1988@users.noreply.github.com>
2022-07-29 23:35:19 -0300

    Modificacion de cross origin

--- src/main/java/com/example/AP/Controller/ProyectoController.java
+++ src/main/java/com/example/AP/Controller/ProyectoController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/proyecto")
-@CrossOrigin(origins = "http://localhost:4200") 
+@CrossOrigin(origins = "https://apmattosfront-b1f8a.web.app") 
 public class ProyectoController {
     
 @Autowired
